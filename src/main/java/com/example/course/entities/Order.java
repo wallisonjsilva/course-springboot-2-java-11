cef8df926562408e24a76af203be197d92817099
Wallison Jr Silva <wallison.cardoso@hotmail.com>
2020-08-11 10:06:17 -0300

    Product-OrderItem one-to-many association

--- src/main/java/com/example/course/entities/Order.java
+++ src/main/java/com/example/course/entities/Order.java
@@ -36,7 +36,6 @@ public class Order implements Serializable {
 	@JoinColumn(name = "client_id")
 	private User client;
 	
-	
 	@OneToMany(mappedBy = "id.order")
 	private Set<OrderItem> items = new HashSet<>();
 	
